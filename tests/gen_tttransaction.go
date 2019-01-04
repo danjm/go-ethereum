ce1d544b191ecaf0cb4d0c3851aaa9f56cc20b76
Dan Miller <danjm.com@gmail.com>
2019-01-04 13:04:35 -0330

    Begin to fix tests in test/TransactionTests, which have been incorrectly passing

--- tests/gen_tttransaction.go
+++ tests/gen_tttransaction.go
@@ -4,8 +4,8 @@ package tests
 
 import (
 	"encoding/json"
-	"errors"
-	"math/big"
+	// "errors"
+	// "math/big"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
@@ -27,15 +27,15 @@ func (t ttTransaction) MarshalJSON() ([]byte, error) {
 		To       common.Address        `gencodec:"required"`
 	}
 	var enc ttTransaction
-	enc.Data = t.Data
-	enc.GasLimit = math.HexOrDecimal64(t.GasLimit)
-	enc.GasPrice = (*math.HexOrDecimal256)(t.GasPrice)
-	enc.Nonce = math.HexOrDecimal64(t.Nonce)
-	enc.Value = (*math.HexOrDecimal256)(t.Value)
-	enc.R = (*math.HexOrDecimal256)(t.R)
-	enc.S = (*math.HexOrDecimal256)(t.S)
-	enc.V = (*math.HexOrDecimal256)(t.V)
-	enc.To = t.To
+	// enc.Data = t.Data
+	// enc.GasLimit = math.HexOrDecimal64(t.GasLimit)
+	// enc.GasPrice = (*math.HexOrDecimal256)(t.GasPrice)
+	// enc.Nonce = math.HexOrDecimal64(t.Nonce)
+	// enc.Value = (*math.HexOrDecimal256)(t.Value)
+	// enc.R = (*math.HexOrDecimal256)(t.R)
+	// enc.S = (*math.HexOrDecimal256)(t.S)
+	// enc.V = (*math.HexOrDecimal256)(t.V)
+	// enc.To = t.To
 	return json.Marshal(&enc)
 }
 
@@ -51,45 +51,45 @@ func (t *ttTransaction) UnmarshalJSON(input []byte) error {
 		V        *math.HexOrDecimal256 `gencodec:"required"`
 		To       *common.Address       `gencodec:"required"`
 	}
-	var dec ttTransaction
-	if err := json.Unmarshal(input, &dec); err != nil {
-		return err
-	}
-	if dec.Data == nil {
-		return errors.New("missing required field 'data' for ttTransaction")
-	}
-	t.Data = *dec.Data
-	if dec.GasLimit == nil {
-		return errors.New("missing required field 'gasLimit' for ttTransaction")
-	}
-	t.GasLimit = uint64(*dec.GasLimit)
-	if dec.GasPrice == nil {
-		return errors.New("missing required field 'gasPrice' for ttTransaction")
-	}
-	t.GasPrice = (*big.Int)(dec.GasPrice)
-	if dec.Nonce == nil {
-		return errors.New("missing required field 'nonce' for ttTransaction")
-	}
-	t.Nonce = uint64(*dec.Nonce)
-	if dec.Value == nil {
-		return errors.New("missing required field 'value' for ttTransaction")
-	}
-	t.Value = (*big.Int)(dec.Value)
-	if dec.R == nil {
-		return errors.New("missing required field 'r' for ttTransaction")
-	}
-	t.R = (*big.Int)(dec.R)
-	if dec.S == nil {
-		return errors.New("missing required field 's' for ttTransaction")
-	}
-	t.S = (*big.Int)(dec.S)
-	if dec.V == nil {
-		return errors.New("missing required field 'v' for ttTransaction")
-	}
-	t.V = (*big.Int)(dec.V)
-	if dec.To == nil {
-		return errors.New("missing required field 'to' for ttTransaction")
-	}
-	t.To = *dec.To
+	// var dec ttTransaction
+	// if err := json.Unmarshal(input, &dec); err != nil {
+	// 	return err
+	// }
+	// if dec.Data == nil {
+	// 	return errors.New("missing required field 'data' for ttTransaction")
+	// }
+	// t.Data = *dec.Data
+	// if dec.GasLimit == nil {
+	// 	return errors.New("missing required field 'gasLimit' for ttTransaction")
+	// }
+	// t.GasLimit = uint64(*dec.GasLimit)
+	// if dec.GasPrice == nil {
+	// 	return errors.New("missing required field 'gasPrice' for ttTransaction")
+	// }
+	// t.GasPrice = (*big.Int)(dec.GasPrice)
+	// if dec.Nonce == nil {
+	// 	return errors.New("missing required field 'nonce' for ttTransaction")
+	// }
+	// t.Nonce = uint64(*dec.Nonce)
+	// if dec.Value == nil {
+	// 	return errors.New("missing required field 'value' for ttTransaction")
+	// }
+	// t.Value = (*big.Int)(dec.Value)
+	// if dec.R == nil {
+	// 	return errors.New("missing required field 'r' for ttTransaction")
+	// }
+	// t.R = (*big.Int)(dec.R)
+	// if dec.S == nil {
+	// 	return errors.New("missing required field 's' for ttTransaction")
+	// }
+	// t.S = (*big.Int)(dec.S)
+	// if dec.V == nil {
+	// 	return errors.New("missing required field 'v' for ttTransaction")
+	// }
+	// t.V = (*big.Int)(dec.V)
+	// if dec.To == nil {
+	// 	return errors.New("missing required field 'to' for ttTransaction")
+	// }
+	// t.To = *dec.To
 	return nil
 }
--- tests/transaction_test_util.go
+++ tests/transaction_test_util.go
@@ -17,116 +17,164 @@
 package tests
 
 import (
-	"bytes"
-	"errors"
+	// "bytes"
 	"fmt"
 	"math/big"
-
-	"github.com/ethereum/go-ethereum/common"
+	"strings"
+	// "log"
+	"encoding/json"
+	"reflect"
+	"errors"
+	// "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/rlp"
 )
 
 // TransactionTest checks RLP decoding and sender derivation of transactions.
-type TransactionTest struct {
-	json ttJSON
-}
 
-type ttJSON struct {
-	BlockNumber math.HexOrDecimal64 `json:"blockNumber"`
-	RLP         hexutil.Bytes       `json:"rlp"`
-	Sender      hexutil.Bytes       `json:"sender"`
-	Transaction *ttTransaction      `json:"transaction"`
+type TransactionTest struct {
+	Byzantium struct {
+		Sender      string       `json:"sender"`
+		Hash        string      	 `json:"hash"`
+	} `json:"Byzantium"`
+	Constantinople struct {
+		Sender      string       `json:"sender"`
+		Hash        string      	 `json:"hash"`
+	} `json:"Constantinople"`
+	EIP150         struct {
+		Sender      string       `json:"sender"`
+		Hash        string      	 `json:"hash"`
+	} `json:"EIP150"`
+	EIP158         struct {
+		Sender      string       `json:"sender"`
+		Hash        string      	 `json:"hash"`
+	} `json:"EIP158"`
+	Frontier       struct {
+		Sender      string       `json:"sender"`
+		Hash        string      	 `json:"hash"`
+	} `json:"Frontier"`
+	Homestead      struct {
+		Sender      string       `json:"sender"`
+		Hash        string      	 `json:"hash"`
+	} `json:"Homestead"`
+	RLP            hexutil.Bytes  `json:"rlp"`
 }
 
 //go:generate gencodec -type ttTransaction -field-override ttTransactionMarshaling -out gen_tttransaction.go
 
 type ttTransaction struct {
-	Data     []byte         `gencodec:"required"`
-	GasLimit uint64         `gencodec:"required"`
-	GasPrice *big.Int       `gencodec:"required"`
-	Nonce    uint64         `gencodec:"required"`
-	Value    *big.Int       `gencodec:"required"`
-	R        *big.Int       `gencodec:"required"`
-	S        *big.Int       `gencodec:"required"`
-	V        *big.Int       `gencodec:"required"`
-	To       common.Address `gencodec:"required"`
+	Sender      hexutil.Bytes       `json:"sender"`
+	Hash        hexutil.Bytes       `json:"has"`
 }
 
 type ttTransactionMarshaling struct {
-	Data     hexutil.Bytes
-	GasLimit math.HexOrDecimal64
-	GasPrice *math.HexOrDecimal256
-	Nonce    math.HexOrDecimal64
-	Value    *math.HexOrDecimal256
-	R        *math.HexOrDecimal256
-	S        *math.HexOrDecimal256
-	V        *math.HexOrDecimal256
+	Sender     hexutil.Bytes
+	Hash       hexutil.Bytes
 }
 
 func (tt *TransactionTest) Run(config *params.ChainConfig) error {
-	tx := new(types.Transaction)
-	if err := rlp.DecodeBytes(tt.json.RLP, tx); err != nil {
-		if tt.json.Transaction == nil {
-			return nil
+	// log.Print("MainnetChainConfig- ", params.MainnetChainConfig)
+	// var cases map[string]interface{}
+
+	// if err := json.Unmarshal(tt, &cases); err != nil {
+	//   fmt.Errorf("Unmarshalling JSON failed: %v", err)
+	// }
+	// fmt.Println(cases)
+	var configInterface map[string]interface{}
+  conf, _ := json.Marshal(params.MainnetChainConfig)
+  json.Unmarshal(conf, &configInterface)
+  // log.Print("configInterface- ", configInterface, " | ", reflect.TypeOf(tt))
+
+	var testDataInterface map[string]interface{}
+  td, _ := json.Marshal(tt)
+  json.Unmarshal(td, &testDataInterface)
+  // log.Print("testDataInterface- ", testDataInterface, " | ", reflect.TypeOf(tt))
+
+	var forkName string
+	var fieldBlockNumber float64
+	testData := reflect.Indirect(reflect.ValueOf(tt))
+	for i := 0; i < testData.NumField(); i++ {
+		forkName = testData.Type().Field(i).Name
+		// log.Print("forkName- ", forkName)
+		if forkName != "RLP" {
+			if configInterface[strings.ToLower(forkName) + "Block"] == nil {
+				fieldBlockNumber = 0
+			} else {
+				fieldBlockNumber = configInterface[strings.ToLower(forkName) + "Block"].(float64)
+			}
+			// log.Print("fieldBlockNumber- ", fieldBlockNumber)
+			tx := new(types.Transaction)
+			// log.Print("tt- ", tt.RLP)
+			forkTestExpectation := testDataInterface[forkName].(map[string]interface{})
+			// log.Print("forkTestExpectation- ", forkTestExpectation)
+
+			expectedSender := forkTestExpectation["sender"]
+			noSenderExpected := forkTestExpectation["sender"] == ""
+			// log.Print("####### expectedSender- ", reflect.TypeOf(expectedSender))
+			if err := rlp.DecodeBytes(tt.RLP, tx); err != nil {
+				if noSenderExpected {
+					return nil
+				}
+				return fmt.Errorf("RLP decoding failed: %v", err)
+			}
+			// log.Print("tx- ", reflect.TypeOf(tx))
+			signer := types.MakeSigner(config, new(big.Int).SetUint64(uint64(fieldBlockNumber)))
+			sender, err := types.Sender(signer, tx)
+			// log.Print("sender- ", sender, " ", err)
+			if err != nil || sender != expectedSender {
+				return err
+			}
+			// err = tt.Transaction.verify(signer, tx)
+			if noSenderExpected && err == nil {
+				return errors.New("field validations succeeded but should fail")
+			}
+			if !noSenderExpected && err != nil {
+				return fmt.Errorf("field validations failed after RLP decoding: %s", err)
+			}
 		}
-		return fmt.Errorf("RLP decoding failed: %v", err)
 	}
-	// Check sender derivation.
-	signer := types.MakeSigner(config, new(big.Int).SetUint64(uint64(tt.json.BlockNumber)))
-	sender, err := types.Sender(signer, tx)
-	if err != nil {
-		return err
-	}
-	if sender != common.BytesToAddress(tt.json.Sender) {
-		return fmt.Errorf("Sender mismatch: got %x, want %x", sender, tt.json.Sender)
-	}
+	// if sender != common.BytesToAddress(tt.Sender) {
+	// 	return fmt.Errorf("Sender mismatch: got %x, want %x", sender, tt.Sender)
+	// }
 	// Check decoded fields.
-	err = tt.json.Transaction.verify(signer, tx)
-	if tt.json.Sender == nil && err == nil {
-		return errors.New("field validations succeeded but should fail")
-	}
-	if tt.json.Sender != nil && err != nil {
-		return fmt.Errorf("field validations failed after RLP decoding: %s", err)
-	}
+	// log.Print("err--- ", err)
 	return nil
 }
 
 func (tt *ttTransaction) verify(signer types.Signer, tx *types.Transaction) error {
-	if !bytes.Equal(tx.Data(), tt.Data) {
-		return fmt.Errorf("Tx input data mismatch: got %x want %x", tx.Data(), tt.Data)
-	}
-	if tx.Gas() != tt.GasLimit {
-		return fmt.Errorf("GasLimit mismatch: got %d, want %d", tx.Gas(), tt.GasLimit)
-	}
-	if tx.GasPrice().Cmp(tt.GasPrice) != 0 {
-		return fmt.Errorf("GasPrice mismatch: got %v, want %v", tx.GasPrice(), tt.GasPrice)
-	}
-	if tx.Nonce() != tt.Nonce {
-		return fmt.Errorf("Nonce mismatch: got %v, want %v", tx.Nonce(), tt.Nonce)
-	}
-	v, r, s := tx.RawSignatureValues()
-	if r.Cmp(tt.R) != 0 {
-		return fmt.Errorf("R mismatch: got %v, want %v", r, tt.R)
-	}
-	if s.Cmp(tt.S) != 0 {
-		return fmt.Errorf("S mismatch: got %v, want %v", s, tt.S)
-	}
-	if v.Cmp(tt.V) != 0 {
-		return fmt.Errorf("V mismatch: got %v, want %v", v, tt.V)
-	}
-	if tx.To() == nil {
-		if tt.To != (common.Address{}) {
-			return fmt.Errorf("To mismatch when recipient is nil (contract creation): %x", tt.To)
-		}
-	} else if *tx.To() != tt.To {
-		return fmt.Errorf("To mismatch: got %x, want %x", *tx.To(), tt.To)
-	}
-	if tx.Value().Cmp(tt.Value) != 0 {
-		return fmt.Errorf("Value mismatch: got %x, want %x", tx.Value(), tt.Value)
-	}
+	// if !bytes.Equal(tx.Data(), tt.Data) {
+	// 	return fmt.Errorf("Tx input data mismatch: got %x want %x", tx.Data(), tt.Data)
+	// }
+	// if tx.Gas() != tt.GasLimit {
+	// 	return fmt.Errorf("GasLimit mismatch: got %d, want %d", tx.Gas(), tt.GasLimit)
+	// }
+	// if tx.GasPrice().Cmp(tt.GasPrice) != 0 {
+	// 	return fmt.Errorf("GasPrice mismatch: got %v, want %v", tx.GasPrice(), tt.GasPrice)
+	// }
+	// if tx.Nonce() != tt.Nonce {
+	// 	return fmt.Errorf("Nonce mismatch: got %v, want %v", tx.Nonce(), tt.Nonce)
+	// }
+	// v, r, s := tx.RawSignatureValues()
+	// if r.Cmp(tt.R) != 0 {
+	// 	return fmt.Errorf("R mismatch: got %v, want %v", r, tt.R)
+	// }
+	// if s.Cmp(tt.S) != 0 {
+	// 	return fmt.Errorf("S mismatch: got %v, want %v", s, tt.S)
+	// }
+	// if v.Cmp(tt.V) != 0 {
+	// 	return fmt.Errorf("V mismatch: got %v, want %v", v, tt.V)
+	// }
+	// if tx.To() == nil {
+	// 	if tt.To != (common.Address{}) {
+	// 		return fmt.Errorf("To mismatch when recipient is nil (contract creation): %x", tt.To)
+	// 	}
+	// } else if *tx.To() != tt.To {
+	// 	return fmt.Errorf("To mismatch: got %x, want %x", *tx.To(), tt.To)
+	// }
+	// if tx.Value().Cmp(tt.Value) != 0 {
+	// 	return fmt.Errorf("Value mismatch: got %x, want %x", tx.Value(), tt.Value)
+	// }
 	return nil
 }
